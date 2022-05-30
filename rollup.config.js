bfb41086261a60a594b956a04c41cb2ae8b06952
lwan <long.wan@elasticpath.com>
2022-05-30 16:25:20 -0700

    Added gradle tasks and GitHub workflow

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ import pkg from './package.json'
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/bundle.js',
+    file: 'dist/bundle.min.js',
     format: 'cjs',
     exports: 'auto'
   },
